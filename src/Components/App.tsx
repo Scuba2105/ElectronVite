ecd0b90d6b82985a2ca4bf31d7e98cd3520100a4
Steven Bradbury <scuba2150@gmail.com>
2024-06-10 09:00:03 +1000

    fixed css

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -5,11 +5,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='page-container'>
-      <div className='main-icon size-100'>Icon</div>
-      <div className='main-header size-100'>Header</div>
-      <div className='main-menu size-100'>Menu</div>
-      <div className='main-area size-100'>Main Area</div>
+    <div className='wrapper'>
+      <div className='main-icon size-100'></div>
+      <div className='main-header size-100'></div>
+      <div className='main-menu size-100'></div>
+      <div className='main-area size-100'></div>
     </div>
   )
 }
