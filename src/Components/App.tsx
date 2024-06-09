aeec469c757bb751152f022bb31bbb3c1053730d
Steven Bradbury <scuba2150@gmail.com>
2024-06-09 19:38:44 +1000

    started styling ui

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,34 +1,16 @@
 import { useState } from 'react'
-import reactLogo from '../Assets/react.svg'
-import viteLogo from '../Assets/electron-vite.animate.svg'
 import '../Styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='page-container'>
+      <div className='main-icon size-100'>Icon</div>
+      <div className='main-header size-100'>Header</div>
+      <div className='main-menu size-100'>Menu</div>
+      <div className='main-area size-100'>Main Area</div>
+    </div>
   )
 }
 
